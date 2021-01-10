62b03e4ecef38d970aef61e24a29ad25ce030099
Karl Pawlowicz <karlpawlowicz@gmail.com>
2021-01-10 02:01:51 -0500

    Start cleaning up

--- README.md
+++ README.md
@@ -5,12 +5,10 @@
   </a>
 </p>
 <h1 align="center">
-  Gatsby's default starter
+  Gatsby Redux Styled Components Starter
 </h1>
 
-Kick off your project with this default boilerplate. This starter ships with the main Gatsby configuration files you might need to get up and running blazing fast with the blazing fast app generator for React.
-
-_Have another more specific idea? You may want to check out our vibrant collection of [official and community-created starters](https://www.gatsbyjs.com/docs/gatsby-starters/)._
+A simple Gatsby starter with Redux, Redux Thunk middleware and styled-components that you can deploy to AWS or Netlify. This starter also includes some basic Prettier and VS Code configuration files and uses Airbnb’s ESLint config.
 
 ## 🚀 Quick start
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,43 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Gatsby Redux Styled Components Starter',
+    description:
+      'A simple Gatsby starter with Redux, Redux Thunk middleware and styled-components that you can deploy to AWS or Netlify. This starter also includes some basic Prettier and VS Code configuration files and uses Airbnb’s ESLint config.',
+    author: '@gatsbyjs',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     {
       resolve: 'gatsby-plugin-eslint',
       options: {
-        exclude: /(.cache|node_modules|public)/,
+        exclude: /(.cachenode_modules|public)/,
         stages: ['develop'],
         test: /\.js$|\.jsx$/,
       },
     },
-    `gatsby-plugin-styled-components`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // 'gatsby-plugin-offline',
   ],
 };
